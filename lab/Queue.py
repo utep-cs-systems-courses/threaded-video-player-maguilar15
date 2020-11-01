021fde40b21206a5a1dcc681e6d3d2d3e2fec193
maguilar15 <maguilar15@miners.utep.edu>
2020-11-01 12:04:03 -0700

    🐇 typo @ Sun Nov  1 12:04:03 MST 2020

--- lab/Queue.py
+++ lab/Queue.py
@@ -29,6 +29,39 @@ class SynchronizedQueueSemaphore:
         return item
 
 
+class SynchronizedQueue:
+    def __init__(self):
+        # Queue Initialization
+        self.queue = []
+        # Locks
+        self.lock = Lock()
+        self.emptyCondition = Condition(self.lock)
+        self.fullCondition = Condition(self.lock)
+
+
+    def size(self):
+        return len(self.queue)
+
+
+    def empty(self):
+        with self.lock:
+            return not self.size()
+
+
+    def put(self, item):
+        with self.fullCondition:
+            self.queue.append(item)
+            self.emptyCondition.notify()
+
+
+    def get(self):
+        with self.emptyCondition:
+            while not self.size():
+                self.emptyCondition.wait()
+            item = self.queue.pop(0)
+            self.fullCondition.notify()
+            return item
+
 
 class Queue:
     def __init__(self):
